ab5f7e0a36b7d5d8e7e168cf60906f6e29485ef1
samoyed88 <12130383@me.mcu.edu.tw>
2024-06-01 12:49:03 +0800

    feat(product): 產品卡片樣式實作

---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css',
+})
+export class ProductCardComponent {
+  productName = 'A產品';
+  authors = '作者甲、作者乙、作者丙';
+  company = '博碩文化';
+}
