758491465e7cca32861c89465aaff92b15f596b9
samoyed88 <12130383@me.mcu.edu.tw>
2024-06-01 22:28:09 +0800

    feat(product):利用文字插值顯示產品名稱、作者與公司資訊，利用屬性繫結實作圖片位址繫結

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -11,4 +11,5 @@ export class ProductCardComponent {
   productName = 'A產品';
   authors = '作者甲、作者乙、作者丙';
   company = '博碩文化';
+  imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
 }
