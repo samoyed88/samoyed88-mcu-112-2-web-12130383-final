a1b42c21b32865862256aa6d6e84bd2accb8fb7d
samoyed88 <12130383@me.mcu.edu.tw>
2024-06-05 11:07:58 +0800

    feat(router): 切換產品清單實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ProductPageComponent],
+  imports: [HeaderComponent, ProductPageComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
