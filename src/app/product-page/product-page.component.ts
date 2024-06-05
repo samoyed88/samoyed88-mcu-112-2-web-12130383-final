0c77e4217220e3786700a80ddab1297e65ff3f37
samoyed88 <12130383@me.mcu.edu.tw>
2024-06-06 00:51:09 +0800

    feat(service): 從服務取得產品資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../model/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -10,47 +11,17 @@ import { ProductCardListComponent } from '../product-card-list/product-card-list
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css',
 })
-export class ProductPageComponent {
-  products = [
-    new Product({
-      id: 1,
-      name: 'A產品',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
-      isSALE: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      id: 2,
-      name: 'B產品',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
-      isSALE: false,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      id: 3,
-      name: 'C產品',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
-      isSALE: false,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      id: 4,
-      name: 'D產品',
-      authors: '作者甲、作者乙、作者丙',
-      company: '博碩文化',
-      isSALE: false,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-  ];
-
+export class ProductPageComponent implements OnInit {
   router = inject(Router);
+
+  private productService!: ProductService;
+
+  products!: Product[];
+
+  ngOnInit(): void {
+    this.productService = new ProductService();
+    this.products = this.productService.getList();
+  }
 
   onView(product: Product): void {
     this.router.navigate(['product', product.id]);
