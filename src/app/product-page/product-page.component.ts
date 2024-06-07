6e89d64906a71dbcaa587b26499fce1c395dafd9
samoyed88 <12130383@me.mcu.edu.tw>
2024-06-08 01:40:35 +0800

    feat(product): 使用 AsynPipe 顯示產品清單資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject, startWith, switchMap } from 'rxjs';
 import { Product } from '../model/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { ProductService } from '../services/product.service';
@@ -7,22 +9,23 @@ import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [ProductCardListComponent],
+  imports: [AsyncPipe, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css',
 })
-export class ProductPageComponent implements OnInit {
+export class ProductPageComponent {
   router = inject(Router);
 
   private productService = inject(ProductService);
 
-  products!: Product[];
-
-  ngOnInit(): void {
-    this.productService.getList().subscribe((products) => (this.products = products));
-  }
+  private readonly refresh$ = new Subject<void>();
 
   onView(product: Product): void {
     this.router.navigate(['product', product.id]);
   }
+
+  readonly products$ = this.refresh$.pipe(
+    startWith(undefined),
+    switchMap(() => this.productService.getList())
+  );
 }
