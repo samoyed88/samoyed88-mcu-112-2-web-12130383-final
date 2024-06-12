c0bfb1e1f114fb4376cf8000f3595e0ad485e069
samoyed88 <12130383@me.mcu.edu.tw>
2024-06-12 16:16:56 +0800

    feat(shopping-cart-page): 新增購物車頁面實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { productResolver } from './resolver/product.resolver';
-import { ShoppingcartPageComponent } from './shoppingcart-page/shoppingcart-page.component';
+import { ShoppingCartPageComponent } from './shopping-cart-page/shopping-cart-page.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products' },
   { path: 'products', component: ProductPageComponent },
   { path: 'product/form/:id', component: ProductDetailPageComponent, resolve: { product: productResolver } },
-  { path: 'login', component: ShoppingcartPageComponent },
+  { path: 'login', component: ShoppingCartPageComponent },
 ];
