56c80ac95c2be1892d2378a4a87c2e764a801a09
samoyed88 <12130383@me.mcu.edu.tw>
2024-06-14 19:52:07 +0800

    feat(shopping-cart-page): 購物車表單實作

--- src/app/interface/shopping-cart.interface.ts
+++ src/app/interface/shopping-cart.interface.ts
@@ -1,7 +1,9 @@
-import { FormControl } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { IOrderForm } from './order-form.interface';
 
-export interface IProductForm {
+export interface IShoppingCartForm {
   name: FormControl<string | null>;
   address: FormControl<string | null>;
   phone: FormControl<string | null>;
+  details: FormArray<FormGroup<IOrderForm>>;
 }
--- src/app/shopping-cart-page/shopping-cart-page.component.ts
+++ src/app/shopping-cart-page/shopping-cart-page.component.ts
@@ -1,20 +1,26 @@
-import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { IProductForm } from '../interface/shopping-cart.interface';
+import { CurrencyPipe, JsonPipe } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IOrderForm } from '../interface/order-form.interface';
+import { IShoppingCartForm } from '../interface/shopping-cart.interface';
+import { Product } from '../model/product';
+import { ShoppingCartService } from '../services/shopping-cart.service';
 
 @Component({
   selector: 'app-shopping-cart-page',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe, CurrencyPipe],
   templateUrl: './shopping-cart-page.component.html',
   styleUrl: './shopping-cart-page.component.css',
 })
-export class ShoppingCartPageComponent {
-  form = new FormGroup<IProductForm>({
+export class ShoppingCartPageComponent implements OnInit {
+  readonly ShoppingCartService = inject(ShoppingCartService);
+
+  form = new FormGroup<IShoppingCartForm>({
     name: new FormControl<string | null>(null, { validators: [Validators.required] }),
     address: new FormControl<string | null>(null, { validators: [Validators.required] }),
     phone: new FormControl<string | null>(null, { validators: [Validators.required, Validators.pattern('\\d{8,10}')] }),
+    details: new FormArray<FormGroup<IOrderForm>>([]),
   });
 
   get name(): FormControl<string | null> {
@@ -27,5 +33,26 @@ export class ShoppingCartPageComponent {
 
   get phone(): FormControl<string | null> {
     return this.form.get('phone') as FormControl<string | null>;
+  }
+
+  get detail(): FormArray<FormGroup<IOrderForm>> {
+    return this.form.get('details') as FormArray<FormGroup<IOrderForm>>;
+  }
+
+  ngOnInit(): void {
+    this.setOrderDetail();
+  }
+
+  setOrderDetail() {
+    for (const item of this.ShoppingCartService.data) {
+      const control = new FormGroup<IOrderForm>({
+        id: new FormControl<number>(item.id, { nonNullable: true }),
+        product: new FormControl<Product>(item.product, { nonNullable: true }),
+        count: new FormControl<number>(item.count, { nonNullable: true }),
+        price: new FormControl<number>(item.product.price * item.count, { nonNullable: true }),
+      });
+
+      this.detail.push(control);
+    }
   }
 }
